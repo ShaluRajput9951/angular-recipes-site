bc30d5e49726e67464f4c73125dd76d91f6b295e
ShaluRajput9951 <shalurajput9951@gmail.com>
2023-10-26 13:34:06 +0530

    added routes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import {  Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
@@ -6,7 +6,6 @@ import { Ingredient } from '../shared/ingredients.model';
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -30,6 +29,10 @@ export class RecipeService {
 
   getRecipes() {
     return this.recipes.slice();
+  }
+  getRecipe(index: number){
+    return this.recipes[index];
+
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
